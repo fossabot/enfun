22fe03100c01eb7da34a96134966e758091a7897
Unknown <matisiek11@gmail.com>
2018-07-16 20:03:59 +0200

    Fixed code styling

--- src/modules/loader/index.js
+++ src/modules/loader/index.js
@@ -2,16 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 const mod = {
-    init({app, config, store}) {
-        this.load({functionsDir: config.functionsDir, store});
-    },
-    load({functionsDir, store}) {
-        store.functions = {};
-        let functionFiles = fs.readdirSync(path.resolve(functionsDir));
-        for (let functionFile of functionFiles) {
-            store.functions[functionFile.replace('.js', '').replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase()] = require(path.join(functionsDir, functionFile));
-        }
-    }
+	init({config, store}) {
+		this.load({functionsDir: config.functionsDir, store});
+	},
+	load({functionsDir, store}) {
+		store.functions = {};
+		let functionFiles = fs.readdirSync(path.resolve(functionsDir));
+		for (let functionFile of functionFiles) {
+			store.functions[functionFile.replace('.js', '').replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase()] = require(path.join(functionsDir, functionFile));
+		}
+	}
 };
 
 export default mod;
