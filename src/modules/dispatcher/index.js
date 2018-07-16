90a1040ca39a63159e4ebb066610d7ecbd0e8acd
Unknown <matisiek11@gmail.com>
2018-07-16 19:55:45 +0200

    Added basic functions loader

--- src/modules/dispatcher/index.js
+++ src/modules/dispatcher/index.js
@@ -1,8 +1,20 @@
-import routes from './routes';
+import express from "express";
+import consola from 'consola';
 
 const mod = {
-	routes
+	init({app, store, config}) {
+		this.bind({app, store, config});
+	},
+    bind({app, store, config}) {
+        const router = express.Router();
+        for(let [functionName, functionHandler] of Object.entries(store.functions)) {
+        	router.all(functionName, (req, res) => {
+				functionHandler.default(req, res);
+			});
+        	consola.info(`Successfully loaded: ${functionName}. It is available on: ${config.endpoint}/${functionName}`);
+		}
+        app.use(config.endpoint, router);
+    }
 };
 
 export default mod;
-module.exports = mod;
